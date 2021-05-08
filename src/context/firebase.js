00265c90d5e9b300b9b270f4a410fe0097069c68
brunchz <info@brunchz.com>
2021-05-08 12:10:38 +0100

    Initial commit

---
+++ src/context/firebase.js
@@ -0,0 +1,9 @@
+import { createContext } from 'react';
+
+const FirebaseContext = createContext(null);
+// Context eliminates the inefficiency of passing props down to components by using
+// providers and consumers that can give access to the firebase data deep inside components.
+export default FirebaseContext;
+
+
+
